27a8ebddb3cd7c1b0e0fea0937389a363e6ebd4f
harshsanas <harrysanas140@gmail.com>
2022-07-21 12:00:44 +0530

    reusable bottom sheet

--- src/components/reusablebottom.jsx
+++ src/components/reusablebottom.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "./reusablebutton.css"
+import Sheet from "react-modal-sheet";
+
 
 export default function ReusableBottom() {
+  const [isOpen,setOpen]=useState(false)
   return (
     <div>
-    <button>OPEN BOTTOM SHEET</button>
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>BOTTOM SHEET OPEN</Sheet.Content>
+        </Sheet.Container>
+
+        <Sheet.Backdrop />
+      </Sheet>
+      <button className="btn-reuablebutton" onClick={() => setOpen(true)}>
+        OPEN BOTTOM SHEET
+      </button>
     </div>
-  )
+  );
 }
